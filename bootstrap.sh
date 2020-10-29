2e940eab9f56c3fed6b41cf5ce1f46e8376fae2e
Jiří Matějíček <jiri.matejicek@safetica.com>
2020-10-29 15:41:23 +0100

    Added local environment files - WIP...

---
+++ bootstrap.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+LGREEN="\e[1;32m"
+LYELLOW="\e[1;33m"
+LBLUE="\e[1;34m"
+LRED="\e[1;31m"
+WHITE="\e[0m"
+
+if [[ $EUID -e 0 ]]; then
+   echo "This script must NOT be run as root" 
+   exit 1
+fi
+
+echo -e "${LBLUE}Enter the system type - local / server / wsl ${WHITE}"
+read SYSTEM_CASE
+
+case $SYSTEM_CASE in
+
+  local)
+    SYSTEM_TYPE=$HOME/dotfiles/local
+    ;;
+
+  server)
+    SYSTEM_TYPE=$HOME/dotfiles/server
+    ;;
+
+  wsl)
+    SYSTEM_TYPE=$HOME/dotfiles/wsl
+    ;;
+
+  *)
+    echo -e "${LYELLOW}--! Unknown system type. Exiting...${WHITE}" 
+    exit 1
+    ;;
+esac
+
+sudo apt install curl wget git zsh autojump 
+
+echo -e "${LGREEN}--> Installing oh-my-zsh ...${WHITE}"
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# plugin zsh-syntax-highlighting
+echo -e "${LGREEN}--> Installing zsh-syntax-highlighting ...${WHITE}"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo -e "${LGREEN}--> Installing zsh-autosuggestions ...${WHITE}"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install powerlevel10K prompt
+echo -e "${LGREEN}--> Installing powerlevel10K prompt ...${WHITE}"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+#sed -i 's/ZSH_THEME=.*/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ~/.zshrc
+
+echo -e "${LGREEN}--> Backup original dot files to ${HOME}/.backup/ ${WHITE}"
+mkdir $HOME/.backup
+
+[[ -f $HOME/.aliases ]] && mv $HOME/.aliases $HOME/.backup/.aliases
+
+[[ -f $HOME/.env ]] && mv $HOME/.env $HOME/.backup/.env
+
+[[ -f $HOME/.gitconfig ]] && mv $HOME/.gitconfig $HOME/.backup/.gitconfig
+
+[[ -f $HOME/.p10k.zsh ]] && mv $HOME/.p10k.zsh $HOME/.backup/.p10k.zsh
+
+[[ -f $HOME/.tmux.conf ]] && mv $HOME/.tmux.conf $HOME/.backup/.tmux.conf
+
+[[ -f $HOME/.zshrc ]] && mv $HOME/.zshrc $HOME/.backup/.zshrc
+
+echo -e "${LGREEN}--> Setting up dot files ...${WHITE}"
+ln -s $SYSTEM_TYPE/.aliases $HOME/.aliases
+ln -s $SYSTEM_TYPE/.env $HOME/.env
+ln -s $SYSTEM_TYPE/.gitconfig $HOME/.gitconfig
+ln -s $SYSTEM_TYPE/.p10k.zsh $HOME/.p10k.zsh
+ln -s $SYSTEM_TYPE/.tmux.conf $HOME/.tmux.conf
+ln -s $SYSTEM_TYPE/.zshrc $HOME/.zshrc
+
+
+echo -e "${LGREEN}--> Changing shell to zsh...${WHITE}"
+chsh -s /usr/bin/zsh
+
+echo -e "${LGREEN}--> Installing basic apps/tools ...${WHITE}"
+source install-tools.sh
+
---
+++ install-tools.sh
@@ -0,0 +1,225 @@
+#!/bin/bash
+#
+# Script for installing/updating some handy tools for backup, encryption, cli etc.
+# - golang
+# - webwormhole
+# - minio-client
+# - azcopy
+# - age
+# - restic
+# - ctop
+# - step
+#
+# Author: JirM
+#
+# Bash strict mode
+#   http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# set -euo pipefail
+# IFS=$'\n\t'
+
+LGREEN="\e[1;32m"
+LYELLOW="\e[1;33m"
+LBLUE="\e[1;34m"
+LRED="\e[1;31m"
+WHITE="\e[0m"
+
+# if [ $EUID -ne 0 ]
+# then
+#    echo -e "${LYELLOW}--! This script needs to be run as root!${WHITE}" 
+#    exit 1
+# fi
+
+app_exists () {
+  if [ -f $1 ]
+  then
+    echo -e "${LBLUE}--> $1 already exists. Updating...${WHITE}"
+    rm -f $1
+  fi
+}
+#app_exists "/usr/local/bin/ww"
+
+install_check () {
+  if [ $2 -eq 0 ]
+  then
+    echo -e "${LGREEN}--> $1 installation successfully completed!${WHITE}"
+  else
+    echo -e "${LRED}--X $1 installation failed. Exiting!${WHITE}"
+    exit 1
+  fi
+}
+#install_check "ctop installation" $?
+
+cmd_check () {
+  if [ $2 -ne 0 ]
+  then
+    echo -e "${LRED}--X $1 failed. Exiting!${WHITE}"
+    exit 1
+  fi
+}
+#cmd_check "Git clone" $?
+
+#apt install curl wget git
+
+# micro
+echo -e "${LGREEN}--> Installing Micro editor...${WHITE}"
+app_exists "/usr/local/bin/micro"
+microfile=$(basename $(curl -s https://api.github.com/repos/zyedidia/micro/releases | grep "browser_download_url.*linux64.tar.gz" | cut -d : -f 2,3 | tr -d \" | head -n 1))
+echo -e "${LGREEN}--> Downloading $microfile...${WHITE}"
+mkdir micro-tmp
+curl -s https://api.github.com/repos/zyedidia/micro/releases | grep "browser_download_url.*linux64.tar.gz" | cut -d : -f 2,3 | tr -d \" | head -n 1 | wget -q -O - -i - | tar -xzf - --strip-components=1 -C ./micro-tmp
+cmd_check "Micro download" $?
+sudo mv "micro-tmp/micro" /usr/local/bin/micro
+sudo rm -rf "micro-tmp/"
+sudo chmod +x /usr/local/bin/micro
+/usr/local/bin/micro --version
+install_check "Micro" $?
+
+# Go
+echo -e "${LGREEN}--> Installing Go...${WHITE}"
+git clone https://github.com/udhos/update-golang
+cmd_check "Git clone" $?
+cd update-golang
+sudo ./update-golang.sh
+cmd_check "Golang install" $?
+cd ..
+sudo rm -rf update-golang
+
+# WebWormhole
+echo -e "${LGREEN}--> Installing WebWormhole...${WHITE}"
+app_exists "/usr/local/bin/ww"
+git clone https://github.com/saljam/webwormhole
+cmd_check "Go clone" $?
+cd webwormhole
+sudo /usr/local/go/bin/go mod download
+cmd_check "Go mod download" $?
+sudo /usr/local/go/bin/go build -o /usr/local/bin/ww ./cmd/ww
+cmd_check "WebWormhole build" $?
+sudo chmod +x /usr/local/bin/ww
+cd ..
+sudo rm -rf webwormhole
+
+/usr/local/bin/ww
+
+if [ $? -eq 2 ]
+then
+  echo -e "${LGREEN}--> Webwormhole installation successfully completed!${WHITE}"
+else
+  echo -e "${LRED}--X Webwormhole installation failed. Exiting!${WHITE}"
+  exit 1
+fi
+
+# Minio-client
+echo -e "${LGREEN}--> Installing minio-client...${WHITE}"
+app_exists "/usr/local/bin/mc"
+sudo curl -s https://dl.min.io/client/mc/release/linux-amd64/mc -o /usr/local/bin/mc
+cmd_check "Minio-client download" $?
+sudo chmod +x /usr/local/bin/mc
+
+# Setup S2 Minio-Client config
+# if [ ! -f /etc/minio/client/config.json ]; then
+#     echo -e "${LBLUE}--> Creating minio-client config file: /etc/minio/client/config.yml${WHITE}"
+#     echo "Input AWS S3 ACCESS_KEY:"
+#     read ACCESS_KEY
+#     echo "Input AWS S3 SECRET_KEY:"
+#     read SECRET_KEY
+#     mkdir -p /etc/minio/client
+# cat >> /etc/minio/client/config.json <<EOF
+# {
+# 	"version": "10",
+# 	"hosts": {
+# 		"gcs": {
+# 			"url": "https://storage.googleapis.com",
+# 			"accessKey": "YOUR-ACCESS-KEY-HERE",
+# 			"secretKey": "YOUR-SECRET-KEY-HERE",
+# 			"api": "S3v2",
+# 			"path": "dns"
+# 		},
+# 		"local": {
+# 			"url": "http://localhost:9000",
+# 			"accessKey": "",
+# 			"secretKey": "",
+# 			"api": "S3v4",
+# 			"path": "auto"
+# 		},
+# 		"play": {
+# 			"url": "https://play.min.io",
+# 			"accessKey": "Q3AM3UQ867SPQQA43P2F",
+# 			"secretKey": "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+# 			"api": "S3v4",
+# 			"path": "auto"
+# 		},
+# 		"s3": {
+# 			"url": "https://s3.amazonaws.com",
+# 			"accessKey": $ACCESS_KEY,
+# 			"secretKey": $SECRET_KEY,
+# 			"api": "S3v4",
+# 			"path": "dns"
+# 		}
+# 	}
+# }
+# EOF
+# fi
+/usr/local/bin/mc -v
+install_check "Minio-client" $?
+
+# AzCopy
+echo -e "${LGREEN}--> Installing azcopy v10...${WHITE}"
+app_exists "/usr/local/bin/azcopy"
+sudo wget -q -O - https://aka.ms/downloadazcopy-v10-linux | tar -xzf - --strip-components=1 -C /usr/local/bin
+cmd_check "AzCopy download" $?
+sudo chmod +x /usr/local/bin/azcopy
+sudo chown root:root /usr/local/bin/azcopy
+/usr/local/bin/azcopy --version
+install_check "AzCopy" $?
+
+# Age
+echo -e "${LGREEN}--> Installing age encryption...${WHITE}"
+app_exists "/usr/local/bin/age"
+agefile=$(basename $(curl -s https://api.github.com/repos/FiloSottile/age/releases | grep "browser_download_url.*linux-amd64.tar.gz" | cut -d : -f 2,3 | tr -d \" | head -n 1))
+echo -e "${LGREEN}--> Downloading $agefile...${WHITE}"
+sudo curl -s https://api.github.com/repos/FiloSottile/age/releases | grep "browser_download_url.*linux-amd64.tar.gz" | cut -d : -f 2,3 | tr -d \" | head -n 1 | wget -q -O - -i - | tar -xzf - --strip-components=1 -C /usr/local/bin
+cmd_check "Age download" $?
+sudo chmod +x /usr/local/bin/age
+sudo chown root:root /usr/local/bin/age
+sudo chmod +x /usr/local/bin/age-keygen
+sudo chown root:root /usr/local/bin/age-keygen
+
+/usr/local/bin/age -h
+
+if [ $? -eq 2 ]
+then
+  echo -e "${LGREEN}--> Age installation successfully completed!${WHITE}"
+else
+  echo -e "${LRED}--X Age installation failed. Exiting!${WHITE}"
+  exit 1
+fi
+
+# Restic
+echo -e "${LGREEN}--> Installing restic backup...${WHITE}"
+app_exists "/usr/local/bin/restic"
+curl -s https://api.github.com/repos/restic/restic/releases | grep "browser_download_url.*linux_amd64.bz2" | cut -d : -f 2,3 | tr -d \" | head -n 1 | wget -q -O - -i - | bzip2 -df > /usr/local/bin/restic
+cmd_check "Restic download" $?
+sudo chmod +x /usr/local/bin/restic
+sudo chown root:root /usr/local/bin/restic
+/usr/local/bin/restic version
+install_check "Restic" $?
+
+# ctop
+echo -e "${LGREEN}--> Installing ctop utility...${WHITE}"
+app_exists "/usr/local/bin/ctop"
+sudo curl -s https://api.github.com/repos/bcicen/ctop/releases | grep "browser_download_url.*linux-amd64" | cut -d : -f 2,3 | tr -d \" | head -n 1 | wget -q -O /usr/local/bin/ctop -i -
+cmd_check "Ctop download" $?
+sudo chmod +x /usr/local/bin/ctop
+sudo chown root:root /usr/local/bin/ctop
+/usr/local/bin/ctop -v
+install_check "Ctop" $?
+
+# step cli: https://github.com/smallstep/cli
+echo -e "${LGREEN}--> Installing Step CLI utility...${WHITE}"
+app_exists "/usr/local/bin/step"
+sudo curl -s https://api.github.com/repos/smallstep/cli/releases | grep "browser_download_url.*linux.*amd64.tar.gz" | cut -d : -f 2,3 | tr -d \" | head -n 1 | wget -q -O - -i - | tar -xzf - --strip-components=2 -C /usr/local/bin
+cmd_check "Step CLI download" $?
+sudo chmod +x /usr/local/bin/step
+sudo chown root:root /usr/local/bin/step
+/usr/local/bin/step version
+install_check "Step" $?
