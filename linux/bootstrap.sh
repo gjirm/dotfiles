96eba88d13aecffd4276d1ffbd4d399e571529f6
Jiri Matejicek <jirm@jirm.cz>
2020-11-13 16:24:57 +0100

    Updated aliases added functions for fd,fzf,micro

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -12,6 +12,39 @@ then
    exit 1
 fi
 
+# Functions:
+
+app_exists () {
+  if [ -f $1 ]
+  then
+    echo -e "${LBLUE}--> $1 already exists. Updating...${WHITE}"
+    rm -f $1
+  fi
+}
+#app_exists "/usr/local/bin/ww"
+
+install_check () {
+  if [ $2 -eq 0 ]
+  then
+    echo -e "${LGREEN}--> $1 installation successfully completed!${WHITE}"
+  else
+    echo -e "${LRED}--X $1 installation failed. Exiting!${WHITE}"
+    exit 1
+  fi
+}
+#install_check "ctop installation" $?
+
+cmd_check () {
+  if [ $2 -ne 0 ]
+  then
+    echo -e "${LRED}--X $1 failed. Exiting!${WHITE}"
+    exit 1
+  fi
+}
+#cmd_check "Git clone" $?
+
+# Functions end
+
 echo -e "${LBLUE}Enter the system profile - personal / server / wsl ${WHITE}"
 read SYSTEM_CASE
 
@@ -35,6 +68,7 @@ case $SYSTEM_CASE in
     ;;
 esac
 
+sudo apt update
 sudo apt install curl wget git file build-essential zsh autojump -y
 #sudo apt instal fd-find
 
@@ -54,18 +88,47 @@ echo -e "${LGREEN}--> Installing powerlevel10K prompt ...${WHITE}"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 #sed -i 's/ZSH_THEME=.*/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ~/.zshrc
 
-echo -e "${LGREEN}--> Installing Fzf ...${WHITE}"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all --no-fish
-
 if [[ "$SYSTEM_CASE" != "server" ]]
 then
+  # Personal, WSL profiles
+
   echo -e "${LGREEN}--> Installing Linuxbrew ...${WHITE}"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
   eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-
+  
+  echo -e "${LGREEN}--> Installing Fzf ...${WHITE}"
+  brew install fzf
+  echo -e "${LGREEN}--> Installing Fd ...${WHITE}"
   brew install fd
+  echo -e "${LGREEN}--> Installing Micro editor ...${WHITE}"
+  brew install micro
+
+else
+  # Server profile
+
+  echo -e "${LGREEN}--> Installing Fzf ...${WHITE}"
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --all --no-fish
+
+  echo -e "${LGREEN}--> Installing Fd search...${WHITE}"
+  sudo apt install fd-find
+  [[ -f $HOME/.zshrc_local ]] || touch $HOME/.zshrc_local 
+  echo "alias fd=fdfind" >> $HOME/.zshrc_local
+  
+  echo -e "${LGREEN}--> Installing Micro editor...${WHITE}"
+  app_exists "/usr/local/bin/micro"
+  microfile=$(basename $(curl -s https://api.github.com/repos/zyedidia/micro/releases | grep "browser_download_url.*linux64.tar.gz" | cut -d : -f 2,3 | tr -d \" | head -n 1))
+  echo -e "${LGREEN}--> Downloading $microfile...${WHITE}"
+  mkdir micro-tmp
+  curl -s https://api.github.com/repos/zyedidia/micro/releases | grep "browser_download_url.*linux64.tar.gz" | cut -d : -f 2,3 | tr -d \" | head -n 1 | wget -q -O - -i - | tar -xzf - --strip-components=1 -C ./micro-tmp
+  cmd_check "Micro download" $?
+  sudo mv "micro-tmp/micro" /usr/local/bin/micro
+  rm -rf "micro-tmp/"
+  sudo chmod +x /usr/local/bin/micro
+  /usr/local/bin/micro --version
+  install_check "Micro" $?
+
 fi
 
 echo -e "${LGREEN}--> Changing shell to zsh...${WHITE}"
