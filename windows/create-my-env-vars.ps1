8e6a174c0e2390f23691c4dfe51b14df614fb280
Jiri Matejicek <jirm@jirm.cz>
2020-11-27 13:41:20 +0100

    Added work git path setting

--- windows/create-my-env-vars.ps1
+++ windows/create-my-env-vars.ps1
@@ -12,6 +12,12 @@ if ($setWorkEnv) {
   Write-Host "--> Variable MY_WORK_PATH set" -ForegroundColor Green
 }
 
+$setWorkGirEnv = Read-Host -Prompt "--> Set ENV variable with path to folder with WORK GIT (MY_GITWORK_PATH)"
+if ($setWorkGirEnv) {
+  [Environment]::SetEnvironmentVariable("MY_GITWORK_PATH", "$setWorkGirEnv","user")
+  Write-Host "--> Variable MY_GITWORK_PATH set" -ForegroundColor Green
+}
+
 $setAppsEnv = Read-Host -Prompt "--> Set ENV variable with path to the my portable APPS folder (MY_GIT_PATH)"
 if ($setAppsEnv) {
   [Environment]::SetEnvironmentVariable("MY_APPS_PATH", "$setAppsEnv","user")
