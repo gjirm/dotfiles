4698369fbf91ff6fe77041355956466846a44ce0
Jiri Matejicek <jirm@jirm.cz>
2025-01-30 15:03:42 +0100

    feat(windows): remove choco and w10 wsl scripts

--- windows/bootstrap-choco.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
--- windows/enable-wsl2-w10.ps1
+++
@@ -1,5 +0,0 @@
-#Requires -RunAsAdministrator
-
-# On Windows 10
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-wsl --set-default-version 2
